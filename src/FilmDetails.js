aa56c02800a9eb3df2ef477e5e157ca761374a5d
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-27 17:44:46 -0400

    Map films to create a listing for each

--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const FilmDetails = () => {
+const FilmDetails = ({films}) => {
+  
   return(
     <div className="film-details">
       <h1 className="section-title">DETAILS</h1>
--- src/FilmListing.js
+++ src/FilmListing.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const FilmListing = () => {
+const FilmListing = ({films}) => {
+  const filmElements = films.map((film, i) => 
+    <li>{film.title}</li>
+  );
+
   return(
     <div className="film-list">
       <h1 className="section-title">FILMS</h1>
+      <ul className="films">
+        {filmElements}
+      </ul>
     </div>
   );
 };
