c23a8e82004fa55457bc3a26d191dd68b685aa11
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-28 14:49:37 -0400

    Console log filterchange on click

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -2,14 +2,31 @@ import React from 'react';
 import FilmRow from './FilmRow';
 
 const FilmListing = ({films, onFaveToggle}) => {
-  const filmElements = films.map((film, i) => 
+
+  const filmRows = films.map((film, i) => 
     <FilmRow key={film.id} film={film} onFaveToggle={onFaveToggle}/>
   );
+
+  const handleFilterClick = (e, selectedFilter) => {
+    e.stopPropagation();
+    console.log(`Setting filter to ${selectedFilter}`);
+  }
 
   return(
     <div className="film-list">
       <h1 className="section-title">FILMS</h1>
-      {filmElements}
+
+      <div className="film-list-filters">
+        <div className="film-list-filter" onClick={(e) => handleFilterClick(e, 'all')}>
+          ALL
+          <span className="section-count">{films.length}</span>
+        </div>
+        <div className="film-list-filter" onClick={(e) => handleFilterClick(e, 'faves')}>
+          FAVES
+          <span className="section-count">0</span>
+        </div>
+      </div>
+      {filmRows}
     </div>
   );
 };
