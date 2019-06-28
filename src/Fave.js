0f6f049fb5036d5c4cb2190aeb7c8fdaa92a28ae
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-27 22:46:31 -0400

    Began function to handle Fave click

--- src/Fave.js
+++ src/Fave.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Fave = () => {
+  const handleClick = (e) => {
+    console.log(`Handling Click!`)
+  }
   return(
     <div className="film-row-fave add_to_queue">
-      <i className="material-icons">add_to_queue</i>
+      <i className="material-icons" onClick={handleClick}>add_to_queue</i>
     </div>
   );
 };
