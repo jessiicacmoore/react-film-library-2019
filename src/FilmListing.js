56f5162a78f5d0f642ad156162d30e5e277238c9
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-29 09:50:58 -0400

    Found better way to toggle 'is-active' in classList

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -5,22 +5,17 @@ const FilmListing = ({films, onFaveToggle}) => {
   const [filter, setFilter] = useState('all');
   const allFilterRef = useRef();
   const favesFilterRef = useRef();
-  const filterRefs = [allFilterRef, favesFilterRef]
 
   const handleFilterClick = (e, selectedFilter) => {
     e.stopPropagation();
     console.log(`Setting filter to ${selectedFilter}`);
     setFilter(selectedFilter);
-    let filterRef = (selectedFilter === 'all') ? allFilterRef : favesFilterRef;
-    filterRefs.forEach(ref => ref.current.classList.remove('is-active'));
-    filterRef.current.classList.add('is-active');
   };
    
   const handleDetailsClick = (film) => {
     console.log(`Fetching details for ${film.title}`)
   };
 
-  
   const filmRows = films.map((film, i) => 
     <FilmRow key={film.id} film={film} onFaveToggle={onFaveToggle} handleDetailsClick={handleDetailsClick}/>
   );
@@ -30,11 +25,11 @@ const FilmListing = ({films, onFaveToggle}) => {
       <h1 className="section-title">FILMS</h1>
 
       <div className="film-list-filters">
-        <div ref={allFilterRef} className="film-list-filter is-active" onClick={(e) => handleFilterClick(e, 'all')}>
+        <div ref={allFilterRef} className={`film-list-filter${(filter === 'all') ? ' is-active' : ''}`} onClick={(e) => handleFilterClick(e, 'all')}>
           ALL
           <span className="section-count">{films.length}</span>
         </div>
-        <div ref={favesFilterRef} className="film-list-filter" onClick={(e) => handleFilterClick(e, 'faves')}>
+        <div ref={favesFilterRef} className={`film-list-filter${(filter === 'faves') ? ' is-active' : ''}`} onClick={(e) => handleFilterClick(e, 'faves')}>
           FAVES
           <span className="section-count">0</span>
         </div>
